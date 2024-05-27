04a7a5e00dfc88788ad1fb0943112f51deed8222
02dejulho <lucianamts.blog@gmail.com>
2024-05-27 17:20:24 -0300

    Iniciando projeto, aplicando migrations e inserindo tabelas no banco de dados

---
+++ Models/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using WowLanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WowLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
