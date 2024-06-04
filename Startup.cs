a8a40be7c508cd78f7cef8815b8008f62098bc0c
02dejulho <lucianamts.blog@gmail.com>
2024-06-04 16:40:54 -0300

    Configurando Session e HttpContext

--- Startup.cs
+++ Startup.cs
@@ -16,13 +16,25 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+
+        services.Configure<CookiePolicyOptions>(options => //trecho para politica de privacidade de cookies, retirado do trabalho do Senac
+        {
+            options.CheckConsentNeeded = context => false;
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+        });
+
+
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddTransient<ILancheRepository, LancheRepository>();
             services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +54,10 @@ public class Startup
 
         app.UseStaticFiles();
         app.UseRouting();
+
+        app.UseCookiePolicy();
+        app.UseSession();
+
         app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
