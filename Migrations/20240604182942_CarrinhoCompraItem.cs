52ef9c31d36c6f8e2070b7877bebbbcc475ec2db
02dejulho <lucianamts.blog@gmail.com>
2024-06-04 15:53:48 -0300

    Criando Carrinho de Compras

---
+++ Migrations/20240604182942_CarrinhoCompraItem.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WowLanches.Migrations
+{
+    public partial class CarrinhoCompraItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarrinhoCompras",
+                columns: table => new
+                {
+                    CarrinhoCompraItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LancheId = table.Column<int>(type: "int", nullable: true),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    CarrinhoCompraId = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoCompras", x => x.CarrinhoCompraItemId);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoCompras_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoCompras_LancheId",
+                table: "CarrinhoCompras",
+                column: "LancheId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoCompras");
+        }
+    }
+}
